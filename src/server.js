1b1ec2cef70932a2a38150e0825fcf92fcc2c673
Robson Ribeiro <robson.ribeiro.silva@accenture.com>
2020-06-07 18:41:58 -0300

    feat: adicionando suporte ao Docker Compose

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello remote on Docker  world!\n');
+  res.send('<h1>Hello world from remote Docker!</h1>');
 });
 
 app.listen(PORT, HOST);
